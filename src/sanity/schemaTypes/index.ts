d2eb11075bf242d2450bb8ab315cd1c0b3df8c12
Will Andree <williamandree@gmail.com>
2026-05-21 10:06:17 -0500

    Removing unused files from template.

--- src/sanity/schemaTypes/index.ts
+++ src/sanity/schemaTypes/index.ts
@@ -5,14 +5,10 @@ import ratesType from './ratesType';
 import tickerType from './tickerType';
 
 // OBJECTS
-import weekdayRates from './objects/weekdayRates';
 import golfPass from './objects/golfPass';
 import miscRates from './objects/miscRates';
 import punchCards from './objects/punchCards';
 import standardRates from './objects/standardRates';
-import weekendRates from './objects/weekendRates';
-import twilightRates from './objects/twilightRates';
-import nonStandardRates from './objects/nonStandardRates';
 
 export const schemaTypes: SchemaTypeDefinition[] = [
   // Documents
@@ -23,8 +19,4 @@ export const schemaTypes: SchemaTypeDefinition[] = [
   miscRates,
   punchCards,
   standardRates,
-  weekdayRates,
-  weekendRates,
-  twilightRates,
-  nonStandardRates,
 ];
